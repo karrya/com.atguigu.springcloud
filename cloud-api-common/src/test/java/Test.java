50b1e42b6e57196ab6af783983efc6145b414efa
likai31841 <946302421@qq.com>
2021-08-16 21:29:11 +0800

    提交新增

---
+++ cloud-api-common/src/test/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Method
+ * @Author likai31841
+ * @Version  1.0
+ * @Date 2021-08-16 21:28
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈！！");
+    }
+}
